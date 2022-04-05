4bfc4f1a80b2e0a399e0cf8d4fa6d64a59340543
Kushal Bhavsar <kushal.inexture@gmail.com>
2022-04-05 12:06:40 +0530

    News Summarization updated.

--- App.py
+++ App.py
@@ -7,9 +7,8 @@ from urllib.request import urlopen
 from newspaper import Article
 import io
 
-st.set_page_config(page_title='InNews: A Summarised News Portal',
-                   # page_icon='icon.ico'
-                   )
+st.set_page_config(page_title='InNews🇮🇳: A Summarised News📰 Portal', page_icon='./Meta/newspaper.ico')
+
 
 def fetch_news_search_topic(topic):
     site = 'https://news.google.com/rss/search?q={}'.format(topic)
@@ -19,6 +18,7 @@ def fetch_news_search_topic(topic):
     sp_page = soup(rd, 'xml')  # scrapping data from site
     news_list = sp_page.find_all('item')  # finding news
     return news_list
+
 
 def fetch_top_news():
     site = 'https://news.google.com/news/rss'
@@ -39,12 +39,13 @@ def fetch_category_news(topic):
     news_list = sp_page.find_all('item')  # finding news
     return news_list
 
+
 def fetch_news_poster(poster_link):
     try:
         u = urlopen(poster_link)
         raw_data = u.read()
         image = Image.open(io.BytesIO(raw_data))
-        image = image.resize((707, 400), )
+        image = image.resize((700, 400), )
         st.image(image, use_column_width=False)
     except:
         image = Image.open('./Meta/no_image.jpg')
@@ -52,11 +53,12 @@ def fetch_news_poster(poster_link):
         st.image(image, use_column_width=False)
 
 
-def display_news(list_of_news,news_quantity):
+def display_news(list_of_news, news_quantity):
     c = 0
     for news in list_of_news:
         c += 1
-        st.markdown(f"({c})[ {news.title.text}]({news.link.text})")
+        # st.markdown(f"({c})[ {news.title.text}]({news.link.text})")
+        st.write('**({}) {}**'.format(c, news.title.text))
         news_data = Article(news.link.text)
         try:
             news_data.download()
@@ -67,16 +69,17 @@ def display_news(list_of_news,news_quantity):
         fetch_news_poster(news_data.top_image)
         with st.expander(news.title.text):
             st.markdown(
-                '''<h5 style='text-align: center; text-justify: inter-word;'>{}"</h5>'''.format(news_data.text),
+                '''<h6 style='text-align: justify;'>{}"</h6>'''.format(news_data.summary),
                 unsafe_allow_html=True)
-            # st.markdown(news_data.text)
-        st.success(news.pubDate.text)
+            st.markdown("[Read more at {}...]({})".format(news.source.text, news.link.text))
+        st.success("Published Date: " + news.pubDate.text)
         if c >= news_quantity:
             break
-def run():
-    st.title("InNews: A Summarised News Portal")
-    image = Image.open('./Meta/newspaper.png')
 
+
+def run():
+    st.title("InNews🇮🇳: A Summarised News📰")
+    image = Image.open('./Meta/newspaper.png')
 
     col1, col2, col3 = st.columns([3, 5, 3])
 
@@ -96,26 +99,37 @@ def run():
         st.subheader("✅ Here is the Trending🔥 news for you")
         no_of_news = st.slider('Number of News:', min_value=5, max_value=25, step=1)
         news_list = fetch_top_news()
-        display_news(news_list,no_of_news)
+        display_news(news_list, no_of_news)
     elif cat_op == category[2]:
-        av_topics = ['Choose Topic','WORLD', 'NATION', 'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT', 'SPORTS', 'SCIENCE', 'HEALTH']
+        av_topics = ['Choose Topic', 'WORLD', 'NATION', 'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT', 'SPORTS', 'SCIENCE',
+                     'HEALTH']
         st.subheader("Choose your favourite Topic")
-        chosen_topic = st.selectbox("Choose your favourite Topic",av_topics)
+        chosen_topic = st.selectbox("Choose your favourite Topic", av_topics)
         if chosen_topic == av_topics[0]:
             st.warning("Please Choose the Topic")
         else:
             no_of_news = st.slider('Number of News:', min_value=5, max_value=25, step=1)
-            st.subheader("✅ Here are the some {} News for you".format(chosen_topic))
             news_list = fetch_category_news(chosen_topic)
-            display_news(news_list, no_of_news)
+            if news_list:
+                st.subheader("✅ Here are the some {} News for you".format(chosen_topic))
+                display_news(news_list, no_of_news)
+            else:
+                st.error("No News found for {}".format(chosen_topic))
 
     elif cat_op == category[3]:
         user_topic = st.text_input("Enter your Topic🔍")
         no_of_news = st.slider('Number of News:', min_value=5, max_value=15, step=1)
-        st.subheader("✅ Here are the some {} News for you".format(user_topic))
-        if st.button("Search"):
+
+        if st.button("Search") and user_topic != '':
             user_topic = user_topic.replace(' ', '')
             news_list = fetch_news_search_topic(topic=user_topic)
-            display_news(news_list,no_of_news)
+            if news_list:
+                st.subheader("✅ Here are the some {} News for you".format(user_topic))
+                display_news(news_list, no_of_news)
+            else:
+                st.error("No News found for {}".format(user_topic))
+        else:
+            st.warning("Please write Topic Name to Search🔍")
+
 
 run()
