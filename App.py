07b6a0ce6ce182e4da2eb655cc367cbd750ea792
JaswanthiShetty123 <113896016+JaswanthiShetty123@users.noreply.github.com>
2022-11-14 18:08:33 +0530

    Update App.py

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ import io
 import nltk
 nltk.download('punkt')
 
-st.set_page_config(page_title='InNews🇮🇳: A Summarised News📰 Portal', page_icon='./Meta/newspaper.ico')
+st.set_page_config(page_title='JSW NEWS: A Summarised News📰 Portal', page_icon='./Meta/newspaper.ico')
 
 
 def fetch_news_search_topic(topic):
